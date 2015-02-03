81c77322861438c8af47a5ce9317164659a99050
runningm <rennenaway@gmail.com>
2015-02-03 15:52:28 -0800

    This better be it
    don't fail me now github

--- MonteCarlo.java
+++ MonteCarlo.java
@@ -1,35 +1,80 @@
-import java.util.Random;
 
-public class MonteCarlo {
-    public double  h,k,r;
+/**
+ * Write a description of class MonteCarlo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.*;
+import java.io.*;
+import java.text.*;
+public class MonteCarlo
+{
+    // instance variables - replace the example below with your own
+    private int x;
 
-    private  Random rndm = new Random();
+    /**
+     * Constructor for objects of class MonteCarlo
+     * @param h is half the length of the square
+     * @param k is half the height of the square
+     * @param r is the radius of the circle
+     */
+    public double h, k, r;
+    private Random rndm = new Random();
+    public MonteCarlo(double h2, double k2, double r2)
+    {
+        // initialise instance variables
+        h = h2;
+        k = k2;
+        r = r2;
 
-    public MonteCarlo(double h,double k, double r){
-        this.h = h;
-        this.k = k;
-        this.r = r;
     }
 
-    public double nextRainDrop_x(){
-        return (rndm.nextDouble()*2*r)+(h-r);
+    /**
+     * Method nextRainDrop_x() gives the x coordinate of a random raindrop within the square
+     * @return     the x corrdinate of the raindrop 
+     */
+    public double nextRain_Drop_x()
+    {
+        // put your code here
+        //Random rndm = new Random();
+        double x = (2*r*rndm.nextDouble()) + (h-r);
+        return x;
     }
 
-    public double nextRainDrop_y(){
-        return (rndm.nextDouble()*2*r)+(k-r);
+    /**
+     * Method nextRainDrop_y() gives the y coordinate of a random raindrop within the square
+     * @return     the y corrdinate of the raindrop 
+     */
+    public double nextRain_Drop_y()
+    {
+        // put your code here
+        //Random rndm = new Random();
+        double y = (2*r*rndm.nextDouble())+(k-r);
+        return y;
     }
-    int cirCount = 0, sqrCount = 0;
-    public boolean insideCirle(double x , double y){
-        if((x < 0)&&(x > 2*r)&&(y<0)&&(y>2*r)){
-            System.out.println("You missed the square!");
-            return false;
 
+    /**
+     * Method insideCircle(double x, double y) tells us whether or not the raindrop is within the circle
+     * @param      double x and double y 
+     * @return     boolean value true if inside or on the cirlce; boolean value false if outside the circle 
+     */
+    public boolean insideCircle(double x, double y)
+
+    {
+        // put your code here
+        boolean b;
+        {
+            if (Math.pow((x-h),2) + Math.pow((y-k),2) <= Math.pow(r,2) && x<(2*h))
+            {
+                b = true;
+            }
+            else
+            {
+                b = false;
+            }
         }
-        else{
-
-            cirCount++;
-            sqrCount++;
-            return Math.pow((x-h), 2)+Math.pow((y-k), 2)<=Math.pow(r,2);
+           return b;
         }
     }
-}
+
--- Tester.java
+++ Tester.java
@@ -1,22 +1,28 @@
+public class tester{
 
-public class Tester {
-static MonteCarlo mcObj = new MonteCarlo(5,3,2);
-static int cirCount=0;
-static int sqrCount =0;
-public static void main(String[] args){
-		
-		for(int i=0; i<10000000;i++){
-			double randx = mcObj.nextRainDrop_x();
-			double randy = mcObj.nextRainDrop_y();
-			//System.out.println(randx);
-			if(mcObj.insideCirle(randx, randy)){
-				cirCount++;
-				
-			}
-			sqrCount++;
-		}
-	
-		System.out.println("Pi is estimated at:  "+ cirCount*Math.pow(2*mcObj.r,2)/(sqrCount*Math.pow(mcObj.r, 2)));
-	}
-	
+    public static void main(String args[]){
+        int cirCount = 0;
+        int sqrCount = 0;
+        MonteCarlo mcObj = new MonteCarlo(5,3,2);
+        for(int i = 0; i<1000000; i++){
+            double randx = mcObj.nextRain_Drop_x();
+            double randy = mcObj.nextRain_Drop_y();
+            //System.out.println(mcObj.insideCircle(randx,randy));
+            //don't need it'll be in the square
+             /*if((randx<0)&&(randx>2*mcObj.r)&&(randy<0)&&(randy>2*mcObj.r)){
+                System.out.println("You have terrible aim! Try hitting the square next time!");
+                sqrCount--;
+            }
+            else*/
+            if(mcObj.insideCircle(randx, randy))
+            {
+                cirCount++;
+
+            }
+            sqrCount++;
+        }
+        //System.out.println(cirCount);
+        //System.out.println(sqrCount);
+        System.out.println("Pi is approx. "+ cirCount*Math.pow(2*mcObj.r,2)/(sqrCount*Math.pow(mcObj.r, 2)));
+    }
 }
